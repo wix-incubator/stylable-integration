b63b2512e4953fdaa8b5daa721029b906952000e
Barak Igal <baraki@lan.wixpress.com>
2017-07-10 16:46:20 +0300

    added some more options and optimizations

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 var path = require('path');
 var loaderUtils = require('loader-utils');
 var stylable = require('stylable');
+var murmurhash = require('murmurhash')
 
-module.exports = function(source) {
-  const options = loaderUtils.getOptions(this);
-  this.addDependency('stylable');  
+const defaults = {
+  namespacelessPrefix: 's'
+};
+
+module.exports = function (source) {
+  const options = Object.assign({}, defaults, loaderUtils.getOptions(this));
+  
+  this.addDependency('stylable');
+  
+  const resourcePath = this.resourcePath;
+  const sheet = stylable.Stylesheet.fromCSS(source);
+  
+  const namespace = (sheet.namespace || options.namespacelessPrefix) + murmurhash.v3(resourcePath);
+
   return `
     Object.defineProperty(exports, "__esModule", { value: true });
-    module.exports.default = require('stylable/react').Stylesheet.fromCSS(${JSON.stringify(source)});
+    const Stylesheet = require('stylable/react').Stylesheet;
+    module.exports = new Stylesheet(${JSON.stringify(sheet.cssDefinition)}, ${JSON.stringify(namespace)});
+    module.exports.default = module.exports;
   `;
+
+
 };
--- test/stylable.spec.js
+++ test/stylable.spec.js
@@ -5,41 +5,77 @@ var webpack = require('webpack');
 var EnvPlugin = require('webpack/lib/web/WebEnvironmentPlugin');
 var _eval = require('node-eval');
 var MemoryFileSystem = require("memory-fs");
-var memfs = new MemoryFileSystem();
+var murmurhash = require('murmurhash')
 
+function testStyleEntry(entry, test, options = {}) {	
+	var memfs = new MemoryFileSystem();
+	webpack({
+		entry: entry,
+		output: {
+			path: "/",
+			filename: 'bundle.js'
+		},
+		plugins: [
+			new EnvPlugin(fs, memfs)
+		],
+		module: {
+			rules: [
+				{
+					test: /\.css$/,
+					loader: path.join(__dirname, '../index'),
+					options: Object.assign({}, options)
+				}
+			]
+		}
+	}, function (err, stats) {
+		test(err, _eval(memfs.readFileSync('/bundle.js', 'utf8')));
+	});
+}
 
 describe('stylable-loader', function () {
 
-	it('should work', function (done) {
-
-		 new Promise(function (res, rej) {
-
-			webpack({
-				entry: path.join(__dirname, './test-main.stylable.css'),
-				output: {
-					path: "/",
-					filename: 'bundle.js'
-				},
-				plugins: [
-					new EnvPlugin(fs, memfs)
-				],
-				module: {
-					rules: [
-						{
-							test: /\.css$/,
-							loader: path.join(__dirname, '../index')
-						}
-					]
-				}
-			}, function (err, stats) {
-				if(err){return done(err)}
-				var _exports = _eval(memfs.readFileSync('/bundle.js', 'utf8'));
-				expect(_exports.classes).to.eql({class: 'class'});
-				done(null);
-			});
-
+	it('export default stylesheet with classes', function (done) {
+		testStyleEntry(path.join(__dirname, './fixtures/test-main.sb.css'), function (err, sheet) {
+			if (err) { return done(err) }
+			expect(sheet.classes).to.eql({ class: 'class' });
+			done(null);
 		});
-
 	});
+
+	it('add hash of from path entry', function (done) {
+		const entry = path.join(__dirname, './fixtures/test-main.sb.css');
+		const hash = murmurhash.v3(entry);
+		testStyleEntry(entry, function (err, sheet) {
+			if (err) { return done(err) }
+			expect(sheet.namespace).to.eql('s' + hash);
+			done(null);
+		});
+	});
+
+	it('prefix namespace to the path hash', function (done) {
+		const entry = path.join(__dirname, './fixtures/test-main-namespace.sb.css');
+		const hash = murmurhash.v3(entry);
+		testStyleEntry(entry, function (err, sheet) {
+			if (err) { return done(err) }
+			expect(sheet.namespace).to.eql('Test' + hash);
+			done(null);
+		});
+	});
+
+	
+	it('prefix namespace to the path hash from the config options', function (done) {
+		const prefix = "Prefix";
+		const entry = path.join(__dirname, './fixtures/test-main.sb.css');
+		const hash = murmurhash.v3(entry);
+		testStyleEntry(entry, function (err, sheet) {
+			if (err) { return done(err) }
+			expect(sheet.namespace).to.eql(prefix + hash);
+			done(null);
+		}, {
+			namespacelessPrefix: prefix
+		});
+	});
+
+
 
 });
