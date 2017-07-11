aa76a8438d1744835eea51e1d36831bf6ce2e988
Barak Igal <baraki@lan.wixpress.com>
2017-07-11 13:27:50 +0300

    add imports as deps

--- index.js
+++ index.js
@@ -19,15 +19,20 @@ function resolveImports(source, context) {
 
 module.exports = function (source) {
   this.addDependency('stylable');
-  
+
   const options = Object.assign({}, defaults, loaderUtils.getOptions(this));
   const resourcePath = this.resourcePath;
   const resolved = resolveImports(source, this.context);
   const sheet = stylable.Stylesheet.fromCSS(resolved);
   const namespace = (sheet.namespace || options.namespacelessPrefix) + murmurhash.v3(resourcePath);
 
+  const imports = sheet.imports.map((importDef) => { 
+    this.addDependency(importDef.from);
+    return createImportString(importDef);
+  });
+
   return `
-    Object.defineProperty(exports, "__esModule", { value: true });
+    Object.defineProperty(exports, "__esModule", { value: true });   
     var Stylesheet = require('stylable/react').Stylesheet;
     var sheet = new Stylesheet(${JSON.stringify(sheet.cssDefinition)}, ${JSON.stringify(namespace)}, ${JSON.stringify(resourcePath)});
     module.exports = sheet;
@@ -35,3 +40,20 @@ module.exports = function (source) {
   `;
 
 };
+
+
+function createImportString(importDef) {
+  var a = [];
+  if(importDef.defaultExport){
+    a.push(importDef.defaultExport);
+  }
+  var b = [];
+  for(var k in importDef.named){
+    a.push(`${importDef.named} as ${k}`);
+  }
+
+  a.push(`{${b.join(',')}}`);
+  
+  
+  return `import ${a.join(', ')} from ${JSON.stringify(importDef.from)}";`;
+}
