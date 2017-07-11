e6813103160ac18e043a3f925575d0a1f1ad9725
Barak Igal <baraki@lan.wixpress.com>
2017-07-11 03:45:43 +0300

    added relative path and source support

--- index.js
+++ index.js
@@ -3,26 +3,35 @@ var loaderUtils = require('loader-utils');
 var stylable = require('stylable');
 var murmurhash = require('murmurhash')
 
+/* must be flat */
 const defaults = {
   namespacelessPrefix: 's'
 };
 
+// const importRegExp = /:import\(["']?(.*?)["']?\)/gm;
+const relativeImportRegExp = /:import\(["']?(\.\/)(.*?)["']?\)/gm;
+
+function resolveImports(source, context) {
+  return source.replace(relativeImportRegExp, (match, rel, thing) => {
+    return match.replace(rel + thing, path.resolve(context, rel + thing));
+  });
+}
+
 module.exports = function (source) {
-  const options = Object.assign({}, defaults, loaderUtils.getOptions(this));
-  
   this.addDependency('stylable');
   
+  const options = Object.assign({}, defaults, loaderUtils.getOptions(this));
   const resourcePath = this.resourcePath;
-  const sheet = stylable.Stylesheet.fromCSS(source);
-  
+  const resolved = resolveImports(source, this.context);
+  const sheet = stylable.Stylesheet.fromCSS(resolved);
   const namespace = (sheet.namespace || options.namespacelessPrefix) + murmurhash.v3(resourcePath);
 
   return `
     Object.defineProperty(exports, "__esModule", { value: true });
-    const Stylesheet = require('stylable/react').Stylesheet;
-    module.exports = new Stylesheet(${JSON.stringify(sheet.cssDefinition)}, ${JSON.stringify(namespace)});
-    module.exports.default = module.exports;
+    var Stylesheet = require('stylable/react').Stylesheet;
+    var sheet = new Stylesheet(${JSON.stringify(sheet.cssDefinition)}, ${JSON.stringify(namespace)}, ${JSON.stringify(resourcePath)});
+    module.exports = sheet;
+    module.exports.default = sheet;
   `;
-
 
 };
--- test/stylable.spec.js
+++ test/stylable.spec.js
@@ -7,7 +7,7 @@ var _eval = require('node-eval');
 var MemoryFileSystem = require("memory-fs");
 var murmurhash = require('murmurhash')
 
-function testStyleEntry(entry, test, options = {}) {	
+function testStyleEntry(entry, test, options = {}) {
 	var memfs = new MemoryFileSystem();
 	webpack({
 		entry: entry,
@@ -28,16 +28,26 @@ function testStyleEntry(entry, test, options = {}) {
 			]
 		}
 	}, function (err, stats) {
-		test(err, _eval(memfs.readFileSync('/bundle.js', 'utf8')));
+		if (err) { throw err; }
+		const bundle = memfs.readFileSync('/bundle.js', 'utf8');
+		test(_eval(bundle), bundle, stats);
 	});
 }
 
 describe('stylable-loader', function () {
 
+
+	it('source path', function (done) {
+		const entry = path.join(__dirname, './fixtures/test-main.sb.css');
+		testStyleEntry(entry, function (sheet, bundle) {
+			expect(sheet.source).to.eql(entry);
+			done(null);
+		});
+	});
+
 	it('export default stylesheet with classes', function (done) {
-		testStyleEntry(path.join(__dirname, './fixtures/test-main.sb.css'), function (err, sheet) {
-			if (err) { return done(err) }
-			expect(sheet.classes).to.eql({ class: 'class' });
+		testStyleEntry(path.join(__dirname, './fixtures/test-main.sb.css'), function (sheet) {
+			expect(sheet.classes).to.contain({ class: 'class' });
 			done(null);
 		});
 	});
@@ -45,8 +55,7 @@ describe('stylable-loader', function () {
 	it('add hash of from path entry', function (done) {
 		const entry = path.join(__dirname, './fixtures/test-main.sb.css');
 		const hash = murmurhash.v3(entry);
-		testStyleEntry(entry, function (err, sheet) {
-			if (err) { return done(err) }
+		testStyleEntry(entry, function (sheet) {
 			expect(sheet.namespace).to.eql('s' + hash);
 			done(null);
 		});
@@ -55,25 +64,37 @@ describe('stylable-loader', function () {
 	it('prefix namespace to the path hash', function (done) {
 		const entry = path.join(__dirname, './fixtures/test-main-namespace.sb.css');
 		const hash = murmurhash.v3(entry);
-		testStyleEntry(entry, function (err, sheet) {
-			if (err) { return done(err) }
+		testStyleEntry(entry, function (sheet) {
 			expect(sheet.namespace).to.eql('Test' + hash);
 			done(null);
 		});
 	});
 
-	
+
 	it('prefix namespace to the path hash from the config options', function (done) {
 		const prefix = "Prefix";
 		const entry = path.join(__dirname, './fixtures/test-main.sb.css');
 		const hash = murmurhash.v3(entry);
-		testStyleEntry(entry, function (err, sheet) {
-			if (err) { return done(err) }
+		testStyleEntry(entry, function (sheet) {
 			expect(sheet.namespace).to.eql(prefix + hash);
 			done(null);
 		}, {
 			namespacelessPrefix: prefix
 		});
+	});
+
+
+	it('should  resolve relative paths', function (done) {
+
+		const entry = path.join(__dirname, './fixtures/import-relative.sb.css');
+		const importPath = path.join(__dirname, './fixtures/my/path');
+
+		testStyleEntry(entry, function (sheet) {
+			expect(sheet.imports[0].from).to.eql(importPath);
+			expect(sheet.imports[1].from).to.eql('@thing');
+			done(null);
+		});
+
 	});
 
 
