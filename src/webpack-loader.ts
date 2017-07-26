c99854e18cde749b0afb6e392ed6d096975c090b
Nadav Abrahami <nadav@wix.com>
2017-07-26 22:51:18 +0300

    workon tests

--- src/webpack-loader.ts
+++ src/webpack-loader.ts
@@ -1,11 +1,18 @@
 import { readFileSync } from 'fs';
 import { transformStylableCSS, defaults } from './stylable-transform';
+import { Stylesheet, Generator, objectifyCSS, Resolver } from 'stylable';
 import { FSResolver } from "./fs-resolver";
 import loaderUtils = require('loader-utils');
+import webpack = require('webpack');
 
-export function loader(this: any, source: string) {
+let firstRun:boolean = true;
+
+const used : any[] = [];
+
+
+export function loader(this:webpack.loader.LoaderContext, source: string) {
     const options = { ...defaults, ...loaderUtils.getOptions(this) };
-
+    console.log('loading')
     const resolver = new FSResolver(options.defaultPrefix);
 
     const { sheet, code } = transformStylableCSS(
@@ -15,12 +22,42 @@ export function loader(this: any, source: string) {
         resolver,
         options
     );
-
+    used.push(sheet)
     this.addDependency('stylable');
 
-    sheet.imports.forEach((importDef: any) => {
-        this.addDependency(importDef.from);
-    });
+    // sheet.imports.forEach((importDef: any) => {
+    //     this.addDependency(importDef.from);
+    // });
+    // if(firstRun){
+    //     firstRun = false;
+    //     this._compiler.plugin('emit',(comp,...args)=>{
+    //         console.log('emitting');
+    //     })
+    // }
 
     return code;
 };
+
+
+export class Plugin{
+    apply(compiler:webpack.Compiler){
+        compiler.plugin('emit',(compilation,callback)=>{
+            const options = { ...defaults };
+            const resolver = new FSResolver(options.defaultPrefix);
+            const gen = new Generator({resolver});
+            used.forEach((s:Stylesheet)=>{
+                 gen.addEntry(s, false);
+            })
+            const resultCssBundle = gen.buffer.join('\n');
+            compilation.assets['bundle.css'] = {
+                source: function(){
+                    return resultCssBundle
+                },
+                size: function(){
+                    return resultCssBundle.length;
+                }
+            }
+            callback();
+        });
+    }
+}
--- test/stylable.spec.ts
+++ test/stylable.spec.ts
@@ -3,10 +3,11 @@ import path = require('path');
 import { expect } from 'chai';
 import webpack = require('webpack');
 import MemoryFileSystem = require('memory-fs');
+
 const EnvPlugin = require('webpack/lib/web/WebEnvironmentPlugin');
 const _eval = require('node-eval');
 const murmurhash = require('murmurhash');
-
+import {Plugin} from '../src/webpack-loader'
 type TestFunction = (evaluated: any, bundle: string, stats?: any) => void
 
 function testStyleEntry(entry: string, test: TestFunction, options = {}) {
@@ -18,13 +19,15 @@ function testStyleEntry(entry: string, test: TestFunction, options = {}) {
 			filename: 'bundle.js'
 		},
 		plugins: [
-			new EnvPlugin(fs, memfs)
+            // new EnvPlugin(fs, memfs),
+            new Plugin()
+
         ],
 		module: {
 			rules: [
 				{
 					test: /\.css$/,
-					loader: path.join(__dirname, '../webpack'),
+					loader: path.join(process.cwd(), '/webpack'),
 					options: Object.assign({}, options)
 				}
 			]
@@ -37,6 +40,87 @@ function testStyleEntry(entry: string, test: TestFunction, options = {}) {
 	});
 }
 
+function testJsEntry(entry: string,files:{[key:string]:string}, test: TestFunction, options = {}) {
+    const memfs = new MemoryFileSystem();
+    Object.keys(files).forEach((filename)=>{
+        memfs.writeFileSync('/'+filename,files[filename]);
+    })
+
+
+    memfs.mkdirpSync('/node_modules/stylable/runtime');
+    memfs.writeFileSync('/node_modules/stylable/runtime/index.js',`
+        module.exports.create = function(rootClass,namespace,namespaceMap,targetCss){
+            return {
+                rootClass,
+                namespace,
+                namespaceMap,
+                targetCss
+            }
+        }
+    `);
+	const compiler = webpack({
+		entry: '/'+entry,
+		output: {
+			path:'/dist',
+			filename: 'bundle.js'
+		},
+		plugins: [
+            new Plugin()
+        ],
+		module: {
+			rules: [
+				{
+					test: /\.css$/,
+					loader: path.join(process.cwd(), '/webpack'),
+					options: Object.assign({}, options)
+				}
+			]
+		}
+    });
+
+
+
+    (compiler as any).inputFileSystem = memfs;
+    (compiler as any).resolvers.normal.fileSystem = memfs;
+    (compiler as any).resolvers.context.fileSystem = memfs;
+    (compiler as any).outputFileSystem = memfs;
+
+
+    compiler.run( function (err: Error, stats: any) {
+        if (err) { throw err; }
+		const bundle = memfs.readFileSync('/dist/bundle.js', 'utf8');
+		const bundleCss = memfs.readFileSync('/dist/bundle.css', 'utf8');
+		test(_eval(bundle), bundleCss, stats);
+    })
+}
+
+
+describe.only('plugin', function(){
+    it('should create modules and target css for css files imported from js',function(done){
+        const files = {
+            'main.js':`
+                module.exports = require("./main.css");
+            `,
+            'main.css':`
+                .gaga{
+                    color:red;
+                }
+            `,
+        }
+        testJsEntry('main.js',files,(bundle,css)=>{
+            const cssModule = bundle.default;
+            console.log(css)
+            const nsCls = cssModule.namespace+'💠gaga'
+            expect(cssModule.rootClass).to.eql('root');
+            expect(cssModule.namespaceMap.gaga).to.eql(nsCls);
+            expect(css).to.eql(`.${nsCls} {\n    color: red\n}`);
+            done();
+        });
+    });
+
+})
+
+
 describe('stylable-loader', function () {
 	it('source path', function (done) {
 		const entry = path.join(__dirname, './fixtures/imports.sb.css');
@@ -46,7 +130,7 @@ describe('stylable-loader', function () {
 			done(null);
 		});
 	});
-	
+
 
 	// it('export default stylesheet with classes', function (done) {
 	// 	testStyleEntry(path.join(__dirname, './fixtures/test-main.sb.css'), function (sheet) {
@@ -103,12 +187,12 @@ describe('stylable-loader', function () {
 	// });
 
 
-	
+
 
 	it('should state from inner dependency', function (done) {
 
 		const entry = path.join(__dirname, './fixtures/3levels/level1.sb.css');
-		
+
 		testStyleEntry(entry, function (sheet, bundle) {
 			expect(bundle).to.match(/[data-secihr9-state]/);
 			done(null);
