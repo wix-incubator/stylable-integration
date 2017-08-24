c04f0a29478f1ffce5e8e9614fe60750d3d97d5b
Barak Igal <baraki@lan.wixpress.com>
2017-08-24 13:25:13 +0300

    added diagnostics reports

--- src/builder.ts
+++ src/builder.ts
@@ -20,6 +20,7 @@ export function build(buildOptions: BuildOptions) {
 
     const fullSrcDir = join(rootDir, srcDir);
     let projectAssets: string[] = [];
+    const diagnosticsMsg: string[] = [];
     const filesToBuild = findFilesRecursive(fs, fullSrcDir, extension);
     const projectAssetMapping: { [key: string]: string } = {};
     filesToBuild.forEach(filePath => {
@@ -29,7 +30,17 @@ export function build(buildOptions: BuildOptions) {
         const content = tryRun(() => fs.readFileSync(filePath).toString(), 'Read File Error');
         const fileDirectory = dirname(filePath);
         const outDirPath = dirname(outPath);
-        const { code } = tryRun(() => transformStylableCSS(content, filePath, resolver, { ...StylableIntegrationDefaults, injectFileCss: true }), 'Transform Error');
+        const { code, sheet } = tryRun(() => transformStylableCSS(content, filePath, resolver, { ...StylableIntegrationDefaults, injectFileCss: true }), 'Transform Error');
+        if(log && sheet.diagnostics.reports.length){
+            diagnosticsMsg.push(`Errors in file: ${filePath}`);
+            sheet.diagnostics.reports.forEach((report)=>{
+                const err = report.node.error(report.message, report.options);
+                diagnosticsMsg.push([
+                    report.message,
+                    err.showSourceCode()
+                ].join('\n'))
+            });
+        }
         const hasDir = fs.existsSync(outDirPath);
         if (!hasDir) {
             tryRun(() => ensureDirectory(outDirPath, fs), 'Ensure directory');
@@ -38,6 +49,11 @@ export function build(buildOptions: BuildOptions) {
         tryRun(() => fs.writeFileSync(outPath, code), 'Write File Error');
         projectAssets = projectAssets.concat(getUsedAssets(content).map((uri: string) => resolve(fileDirectory, uri)));
     });
+
+    if(log && diagnosticsMsg.length){
+        log('[Diagnostics]\n', diagnosticsMsg.join('\n\n'));
+    }
+
     projectAssets.forEach((originalPath: string) => {
         projectAssetMapping[originalPath] = originalPath.replace(join(rootDir, srcDir), join(rootDir, outDir))
     })
