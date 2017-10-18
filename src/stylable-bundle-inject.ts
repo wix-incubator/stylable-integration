3d5274f65ef59f3c6981e0bcc648f6ed3e45172b
Barak Igal <baraki@lan.wixpress.com>
2017-10-18 10:50:10 +0300

    fix module output

--- src/stylable-bundle-inject.ts
+++ src/stylable-bundle-inject.ts
@@ -14,7 +14,7 @@ export class StylableBundleInjector {
 function build(this: any, options: any, compilation: any, resolver: any, _fs: any, callback: any) {
     NormalModule.prototype.build.call(this, options, compilation, resolver, {
         readFile: (_filepath: string, callback: Function) => {
-            var buffy = new Buffer(`module.exports = { ${this.body} };`);
+            var buffy = new Buffer(`${this.body}`);
             callback(null, buffy);
         }
     }, callback);
