f757bdb19a5d5f64f7eddef411e86fb01e478344
Barak Igal <baraki@wix.com>
2018-01-22 12:40:12 +0200

    expose test kit

--- src/stylable-to-module-factory.ts
+++ src/stylable-to-module-factory.ts
@@ -4,11 +4,11 @@ import { createCSSModuleString } from "./stylable-transform";
 import { Stylable } from "stylable";
 
 
-export function stylableToModuleFactory(_fs = fs, _require = require, nsDelimiter?: any) {
+export function stylableToModuleFactory(_fs = fs, _require = require, nsDelimiter?: string) {
   const stylable = new Stylable('root', _fs, _require, nsDelimiter);
   const options = { injectFileCss: true };
 
-  return function stylableToModule(src: any, path: string) {
+  return function stylableToModule(src: string, path: string) {
     let code = src;
     const res = stylable.transform(src, path)
     code = createCSSModuleString(res, options);
